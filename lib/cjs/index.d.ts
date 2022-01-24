c7e641fac37a4c8ce13453b5fe2440779239ae1f
lengfangbing <lengfangbing@gmail.com>
2022-01-24 21:28:01 +0800

    feat: 增加打包产物

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,2 +1,2 @@
 import { BasedValueType, PipeConfigWithCoreFunc, PipeValueConfigType, PipeValueType } from './types';
-export default function createPipe<Value extends BasedValueType, Config extends PipeValueConfigType>(value: Value, pipeFuncConfig: Config): Omit<PipeValueType<Value, PipeConfigWithCoreFunc<Value, Config>, () => BasedValueType>, "pipeEnd">;
+export default function createPipe<Value extends BasedValueType, Config extends PipeValueConfigType<Value>>(value: Value, pipeFuncConfig: Config): Omit<PipeValueType<Value, PipeConfigWithCoreFunc<Value, Config>, () => BasedValueType>, "pipeEnd">;
